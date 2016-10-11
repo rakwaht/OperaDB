9815382257d692fe96825c0b256d416468f712f7
System Administrator <root@MBP-di-Davide.station>
2016-10-11 18:29:31 +0200

    added gitingore

--- opera_backend/seed_sql/clear_db.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE elenco_passaggi;
-DROP TABLE mense;
-DROP TABLE prodotti;
-DROP TABLE tsop_ricarica;
-DROP TABLE tsop_totem_ricarica;
-DROP TABLE v_badge;
-DROP TABLE v_info_conto;
-DROP TABLE v_info_tessera;
--- opera_backend/seed_sql/tsop_totem_ricarica.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tsop_totem_ricarica (
-    `id` INT,
-    `codice` INT,
-    `descrizione` VARCHAR(16) CHARACTER SET utf8,
-    `localita` VARCHAR(27) CHARACTER SET utf8
-);
-INSERT INTO tsop_totem_ricarica VALUES (2,2,'MESIANO','<Trento, locolita'' Mesiano>');
-INSERT INTO tsop_totem_ricarica VALUES (3,3,'POLO TECNOLOGICO','<Povo, via Sommarive>');
-INSERT INTO tsop_totem_ricarica VALUES (1,1,'T. Gar','<Trento, via Tommaso Gar>');
